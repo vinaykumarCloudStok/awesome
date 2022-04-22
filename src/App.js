d61c83b32111112007ac5cc4ead730c953001c02
vinay151 <vinayakkumar388@gmail.com>
2022-04-22 10:31:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import './App.css';
-import Home from './component/Home';
+
+
+import LanguageSelect from './component/language/LanguageSelect';
 
 function App() {
   return (
    <>
-   <Home/>
+
+ <LanguageSelect/>
    </>
   );
 }
